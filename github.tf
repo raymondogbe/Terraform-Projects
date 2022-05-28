d945afabf8610d4808ddf3581ef2d02168a57be8
Raymond <debillion13@gmail.com>
2022-05-28 18:51:54 +0300

    Update github.tf

--- github.tf
+++ github.tf
@@ -10,12 +10,15 @@ provider "github" {
   token = "ghp_gS8t0ossfsdgsgsdgsaF6Wfhgdfgdg4btRRR"
 }
 
-resource "github_repository" "others" {
-  name        = "Other-stuffs"
-  description = "Collection of codes"
+resource "github_repository" "newrepo" {
+  name        = "My_first_repo"
+  description = "Trying out new things with terraform"
 
   visibility = "public"
 }
 
 
-Remember that when creating your token, you must tick access to the token to be able to create and delete token, otherwise, you maybe experiencing 404 error.
+Remember that when creating your token, you must tick the different acesses like repo(to create repo), delete_repo(to delete repo) to the token to be able to create and delete token, otherwise, you maybe experiencing 404 error.
+The resource name should not have spaces
+if you want to delete only a resource using terraform, use the terraform destroy -target "github resource"."resourcename"
+
